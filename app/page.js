a9fb13ce459e9168aa10ceb6af25a62ee83a4b5f
Rocky McGoey <mcgoeyrocky@gmail.com>
2025-09-18 15:30:46 -0600

    changed the structure

--- app/page.js
+++ app/page.js
@@ -2,11 +2,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="p-8">
+    <body>
+    <header>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
+    </header>
+    <main className="p-8">
       <p>
         <Link href="/week-2">Week 2</Link>
       </p>
-      </main>
+    </main>
+    </body>
   );
 }
