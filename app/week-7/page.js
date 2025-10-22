630a86e2ee2af38eb0b698b1a37736def6ef0576
Rocky McGoey <mcgoeyrocky@gmail.com>
2025-10-22 08:08:21 -0600

    Added New-item page from week 5, Added a handleAddItem function

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,11 +1,25 @@
+"use client";
 import ItemList from "./item-list";
+import NewItem from "./new-item";
+import { useState } from "react";
+import itemsData from "./items.json";
+
 
 export default function Page() {
+
+  const [items, setItems] = useState(itemsData);
+
+{/*donesn't mutate array*/}
+  function handleAddItem(newItem) {
+    setItems((prevItems) => [...prevItems, newItem]);
+  }
+
   return (
     <main className="min-h-screen p-6">
       <div className="max-w-2xl mx-auto">
         <h1 className="text-2xl font-bold text-center text-black dark:text-blue-600 mb-6">Shopping List</h1>
-        <ItemList />
+        <NewItem onAddItem={handleAddItem} />
+        <ItemList items={items}/>
       </div>
     </main>
   );
